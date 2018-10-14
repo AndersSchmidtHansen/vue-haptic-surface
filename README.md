b2b703d5a4070790b9b2bd66ce266a8deeee8082
Anders Schmidt Hansen <me@andersschmidt.com>
2018-10-14 20:14:36 +0200

    [Dev] Added first version

--- README.md
+++ README.md
@@ -1,2 +1,138 @@
 # vue-haptic-surface
 A Vue.js component that sends back geometric data from any element it wraps.
+
+## [CodePen Example](https://codepen.io/andersschmidt/full/3c3ee5243ba353d74cc2b2a551dae909/)
+
+![Example .GIF](./example.gif)
+
+## Installation
+```
+npm install vue-haptic-surface
+```
+```
+import VueHapticSurface from 'vue-haptic-surface'
+Vue.use(VueHapticSurface)
+```
+
+## Usage
+```
+<template lang="pug">
+  haptic-surface(@sensing="onSense")
+    .element(:style="{ transform: `translateY(${haptic && haptic.pointer.y}px)` }")
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        haptic: null
+      }
+    },
+    methods: {
+      onSense (data) {
+        this.haptic = data
+      }
+    }
+  }
+</script>
+```
+
+## Quick Tip for nested elements
+If you have multiple elements nested inside a `haptic-surface` component, you may end up in a situation where the positions of those nested elements go bonkers. That is because of the nested elements also triggering the `mousemove` when moving around the surface of the upper-most component.
+
+One way of fixing this is to remove the `pointer-events` from those nested elements using a class like this:
+```
+.pointer-events-none-children > * { pointer-events: none; }
+```
+
+It's just one way of handling that. 
+
+## What data is returned?
+The component exposes a couple of different collections of data, primarily from the `mousemove` and `click` events. They are as follows...
+
+### Pointer (e.g. `haptic.pointer`)
+Simply exposes the current mouse position, including an inverted version of the `x` and `y` values and a 360 degree calculation. The `degree` value is especially useful for rotations and changing the hue of colors on an element.
+
+```
+{
+  "x": 141,
+  "y": 27,
+  "inverted_x": -141,
+  "inverted_y": -27,
+  "degrees": 277
+}
+```
+
+### Center (e.g. `haptic.center`)
+Returns the centerpoints of the `haptic-surface` element.
+```
+{
+  "x": 125,
+  "y": 150,
+  "inverted_x": -125,
+  "inverted_y": -150
+}
+```
+
+### Shape (e.g. `haptic.shape`)
+The basic shape data of the `haptic-surface` component itself. This is just the `getBoundingClientRect()` information without any bell's and whistles.
+
+```
+{
+  "x": 32,
+  "y": 163,
+  "width": 250,
+  "height": 300,
+  "top": 163,
+  "right": 282,
+  "bottom": 463,
+  "left": 32
+}
+```
+
+### Distance (e.g. `haptic.distance`)
+Returns a boatload of information about the pointer's distance from each corner (and the center) of the `haptic-surface` element. Also includes a percentage calculation in both directions which can be useful in cases where having a 0 to 100% value is more optimal.
+
+```
+// haptic.distance.topLeft
+{
+  "x": 141,
+  "y": 27,
+  "inverted_x": -141,
+  "inverted_y": -27,
+  "to_center_x_pct": 100,
+  "to_center_y_pct": 18,
+  "from_center_x_pct": 0,
+  "from_center_y_pct": 82
+}
+```
+
+### Corners (e.g. `haptic.corners`)
+The same as `Shape`, but returns the `x` and `y` points of the corners of the `haptic-surface` element.
+
+```
+{
+  "x": 0,
+  "y": 0,
+  "inverted_x": 0,
+  "inverted_y": 0
+}
+```
+
+### Memory (e.g. `haptic.memory.clicks`)
+Sometimes it's useful to have a list of clicks that happened prior to the current click event. A history of clicks, you could say. It defaults to `5`, but you can set it to whatever using the `maxClickPositionsSaved` prop.
+
+A potential use case is to add ripple effects on buttons or elements, or placing a trail of dots, hotspots or other markers.
+
+```
+{
+  "x": 110,
+  "y": 96,
+  "inverted_x": -110,
+  "inverted_y": -96,
+  "created_at": 1539539165970
+}
+```
+
+## License
+The MIT License
---
+++ src/index.js
@@ -0,0 +1,155 @@
+const VueHapticSurface = {
+  install(Vue, options) {
+    Vue.component("haptic-surface", {
+      template: `<div v-on="$listeners" @mousemove="sense" @click="sense"><slot></slot></div>`,
+      props: {
+        maxClickPositionsSaved: {
+          type: Number,
+          default: 5
+        }
+      },
+      data () {
+        return {
+          memory: {
+            clicks: []
+          },
+          events: {
+            mouse: new MouseEvent(null)
+          }
+        }
+      },
+      computed: {
+        context () {
+          return {
+            shape: this.shape,
+            center: this.center,
+            pointer: this.pointer,
+            distance: this.distance,
+            corners: this.corners,
+            memory: this.memory
+          }
+        },
+        center () {
+          return this.getCoords(this.shape.width / 2, this.shape.height / 2)
+        },
+        distance () {
+          const points = [
+            {
+              type: 'topLeft',
+              coords: this.getCoords(this.pointer.x, this.pointer.y)           
+            },
+            {
+              type: 'topRight',
+              coords: this.getCoords(this.corners.topRight.x - this.pointer.x, this.corners.topRight.y + this.pointer.y)           
+            },
+            {
+              type: 'bottomRight',
+              coords: this.getCoords(this.corners.bottomRight.x - this.pointer.x, this.corners.bottomRight.y - this.pointer.y)           
+            },
+            {
+              type: 'bottomLeft',
+              coords: this.getCoords(this.corners.bottomLeft.x + this.pointer.x, this.corners.bottomLeft.y - this.pointer.y)           
+            }            
+          ]
+          
+          const output = points.reduce((a, b) => {
+            a[b.type] = {
+              ...b.coords,
+              ...this.getDistanceFromCorner(b.type),
+              ...this.getCornerToCenterPercentage(b.coords, this.center)
+            }
+            return a
+          }, {
+            center: {
+              ...this.getCoords(this.pointer.x - this.center.x, this.center.y - this.pointer.y),
+              d: this.getDistance(this.pointer.x, this.center.x, this.pointer.y, this.center.y)
+            }
+          })  
+          return output
+        },
+        corners () {
+          return {
+            topLeft:     this.getCoords(0, 0),
+            topRight:    this.getCoords(this.shape.width, 0),
+            bottomRight: this.getCoords(this.shape.height, this.shape.width),
+            bottomLeft:  this.getCoords(0, this.shape.height)
+          }
+        },
+        shape () {
+          const rect = this.$el.getBoundingClientRect()
+          return rect
+        },
+        pointer () {
+          const {
+            offsetX: x,
+            offsetY: y
+          } = this.events.mouse
+          return {
+            ...this.getCoords(x, y),
+            degrees: this.getPointDegrees(y, this.center.y, x, this.center.x)
+          }
+        }
+      },
+      methods: {
+        clamp (min, max) {
+          return Math.min(Math.max(0, min), max)
+        },
+        getCornerToCenterPercentage (corner, center, clamp = 100) {
+          const clampedPct = (c, axis = 'x', dir = 'getPercentageIncrease') => {
+            return this.clamp(this[dir](corner[axis], c), clamp)
+          }
+          return ({
+            to_center_x_pct: clampedPct(center.x),
+            to_center_y_pct: clampedPct(center.y, 'y'),
+            from_center_x_pct: clampedPct(center.x, 'x', 'getPercentageDecrease'),
+            from_center_y_pct: clampedPct(center.y, 'y', 'getPercentageDecrease')
+          })
+        }, 
+        getDistanceFromCorner (corner = 'topLeft') {
+          if (!this.pointer) return
+          return this.getDistance(this.pointer.x, this.corners[corner].x, this.pointer.y, this.corners[corner].y)
+        },
+        getCoords (x, y) {
+          return ({
+            x,
+            y,
+            inverted_x: x < 0 ? Math.abs(x) : -x,
+            inverted_y: y < 0 ? Math.abs(y) : -y
+          })
+        },
+        getPointDegrees (y, cy, x, cx) {
+          return Math.floor((Math.atan2(y - cy, x - cx) * 180/Math.PI + 360) % 360)
+        },
+        getPercentageIncrease (x, y) {
+          return Math.floor((x / y) * 100)
+        },
+        getPercentageDecrease (x, y) {
+          return Math.floor(((y - x) / y) * 100)
+        },
+        getDistance (x1, x2, y1, y2) {
+          return Math.floor(Math.hypot(x2 - x1, y2 - y1))
+        },
+        setPointerPositionInMemory () {
+          const payload = {
+            ...this.getCoords(this.pointer.x, this.pointer.y),
+            created_at: new Date().getTime()
+          }
+          this.memory.clicks = [payload, ...this.memory.clicks].slice(0, this.maxClickPositionsSaved)
+        },
+        sense (event) {
+          if (event.type.match('mousemove')) {
+            this.events.mouse = event
+          }
+          
+          if (event.type.match('click')) {
+            this.setPointerPositionInMemory()
+          }
+          
+          this.$emit('sensing', this.context)
+        }
+      }
+    })
+  }
+}
+
+export default VueHapticSurface
